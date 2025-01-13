78ae669fef2337e0f914e61f017892ab78f7982a
Franknaw <franknaw@gmail.com>
2025-01-13 11:22:56 -0500

    Update capture_mm.py

--- capture_mm.py
+++ capture_mm.py
@@ -1,1 +1,14 @@
+import mouse
+import pickle
+
+
+def capture_mm():
+    mm_events = mouse.record()
+    f = open('mm_events.txt', 'wb')
+    pickle.dump(mm_events, f)
+    f.close()
+
+
+if __name__ == "__main__":
+    capture_mm()
 
