3df9b19e1438dd4dd25cf4fd5848abecc73016dd
Franknaw <franknaw@gmail.com>
2025-05-21 12:33:41 -0400

    Update run_mm.py

--- run_mm.py
+++ run_mm.py
@@ -7,11 +7,12 @@ import random
 
 import mouse
 
-# Type Annotations
-var1: str = 'mm_events.txt'
-var2: str = 'mm_events1.txt'
-var3: str = 'mm_events2.txt'
-variables: list[str] = [var1, var2, var3]
+# Using Type Annotations
+var1: str = 'events_0.dat'
+var2: str = 'events_1.dat'
+var3: str = 'events_2.dat'
+var4: str = 'events_3.dat'
+variables: list[str] = [var1, var2, var3, var4]
 max_time: float = time.time() + 1200
 
 
@@ -23,7 +24,7 @@ def enable_print() -> None:
     sys.stdout = sys.__stdout__
 
 
-def run_mouse(mm_events, length) -> None:
+def run_mouse(events, length) -> None:
     current_time: float = time.time()
     print("Hello!", flush=True)
     try:
@@ -33,7 +34,7 @@ def run_mouse(mm_events, length) -> None:
                 exit()
             print(f"Working {idx}", flush=True)
             time.sleep(random.randint(5, 10))
-            mouse.play(mm_events[:-1])
+            mouse.play(events[:-1])
             idx += 1
     except KeyboardInterrupt:
         print("Good Bye!")
@@ -46,13 +47,13 @@ def load_events(event_file) -> Any:
 
 
 def randomize_events() -> None:
-    run_mouse(load_events(random.choice(variables)), random.randint(1, 5))
+    run_mouse(load_events(random.choice(variables)), random.randint(1, 3))
 
 
 if __name__ == "__main__":
+    disable_print()
     if len(sys.argv) - 1 > 0:
-        enable_print()
-    else:
-        disable_print()
+        # convert minutes to seconds
+        max_time = time.time() + (int(sys.argv[1]) * 60)
     for i in range(random.randint(10, 20)):
         randomize_events()
